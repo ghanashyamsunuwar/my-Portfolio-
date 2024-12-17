33cdf1fa8e33172092cfeb39d98814892ff4ec2e
ghanashyam sunuwar <janamsunuwar13@gmail.com>
2024-12-17 19:34:24 +0545

    check

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,18 +7,7 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          {/* <p>
-           Hi, I’m GhanaShyam Sunuwar from Kathmandu, Nepal. 
-           <br />
-           I hold a Bachelor’s degree in Computer Applications (BCA) from Triton SS College.
 
-           I’m a passionate React.js frontend developer with expertise in building 
-           engaging user interfaces, crafting seamless user experiences, and exploring 
-           the latest frontend technologies. I work with tools like React.js, Next.js, 
-           Redux, TypeScript, and Styled Components to create innovative web solutions.
-
-           Beyond coding, I enjoy playing games, video/photo editing, and remixing songs.
-          </p> */}
   <p style={{ textAlign: "justify", margin: "0 20px" }}>
       Hi, I’m GhanaShyam Sunuwar from Kathmandu, Nepal. 
       <br />
@@ -33,26 +22,6 @@ function AboutCard() {
       <br />
       Beyond coding, I enjoy playing games, video/photo editing, and remixing songs.
     </p>
-          {/* <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">GhanaShyam Sunuwar </span>
-            from <span className="purple"> Kathmandu, Nepal.</span>
-            <br />
-            I have completed my Bachelor In Computer Application (BCA) from Triton SS College.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p> */}
-          {/* <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Video/Photo Editing
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Remix Song 
-            </li>
-          </ul> */}
 
           <p style={{ color: "#f8b65f" }}>
             "Strive to build things that make a difference!"
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by GhanaShyam Sunuwar</h3>
+          <h3>Designed and Developed by Ghanashyam Sunuwar</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Contact number 9709035532</h3>
